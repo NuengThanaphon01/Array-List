e37e2e00a2dff87b8aa1fec332967cde7b815052
NuengThanaphon01 <nueng.thanaphon01@gmail.com>
2021-03-07 22:05:27 +0700

    Array-List Born to Dev

---
+++ ProjectArrayList/ProjectArrayList/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectArrayList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+        private void btnShowall_Click(object sender, RoutedEventArgs e)
+        {
+            string dataShow = "" ;
+
+            foreach (string data in arrayList)
+                dataShow += data + System.Environment.NewLine;
+
+             MessageBox.Show(dataShow);
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList.Add(txtData.Text);
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList.Remove(txtData.Text);
+        }
+    }
+}
